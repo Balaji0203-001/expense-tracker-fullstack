81c3337c676330c1bc3aabdf83d3b33bd8137eea
Balaji0203-001 <suryawanshibalaji234@gmail.com>
2026-05-10 13:10:53 +0530

    Updated frontend API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://expense-tracker-fullstack-app-1n25.onrender.com/api",
 })
 
 export default axiosInstance
